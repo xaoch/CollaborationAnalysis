e042477bcb7865fce1a1d2b22c8bad94fbffe8ff
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-04-26 16:30:01 -0400

    Slurm script

--- dewarp.s
+++ dewarp.s
@@ -14,6 +14,7 @@ module load python/intel/3.8.6
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
+pip install -U numpy
 pip install opencv-python
 
 OUTDIR=$SCRATCH/dewarped
