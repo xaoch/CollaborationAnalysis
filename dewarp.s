00c686f6afb3c59dccb31a6bfd5984df1265e8ff
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-04-26 16:16:08 -0400

    Slurm script

---
+++ dewarp.s
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=2GB
+#SBATCH --job-name=deWarpingVideo
+#SBATCH --mail-type=END
+#SBATCH --mail-user=xavier.ochoa@nyu.edu
+#SBATCH --output=slurm_videoDeWarp%j.out
+
+module purge
+module load python/intel/3.8.6
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install opencv-python
+
+OUTDIR=$SCRATCH/dewarped
+DATADIR=$SCRATCH/data
+mkdir -p $OUTDIR
+DIR=$USER/Code/CollaborationAnalysis
+cd $DIR
+python dewarp.py $DATADIR/Four.mp4 $OUTDIR/FourDewarped.mp4
