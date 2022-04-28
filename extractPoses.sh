df18d9b557e99b30ca145b0ccd5bbd056d2772e1
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-04-28 11:07:13 -0400

    Extract Posture

---
+++ extractPoses.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/xao1/Code/openpose
+
+mkdir $3
+
+build/examples/openpose/openpose.bin \
+    --video $1 \
+    --write_json $3 \
+    --write_video $2 \
+    --display 0
---
+++ postureOpenPose.s
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=2:00:00
+#SBATCH --mem=4GB
+#SBATCH --job-name=poseExtraction
+#SBATCH --mail-type=END
+#SBATCH --mail-user=xavier.ochoa@nyu.edu
+#SBATCH --output=slurm_poseExtraction%j.out
+
+module purge
+OUTFILE=$2
+OUTJSON=$3
+VIDEOFILE=$1
+singularity run --nv --overlay /scratch/work/public/singularity/openpose1.7.0-cuda11.1.1-cudnn8-devel-ubuntu20.04-dep.sqf:ro /scratch/work/public/singularity/cuda11.1.1-cudnn8-devel-ubuntu20.04.sif /bin/bash /home/xao1/Code/CollaborationAnalytis/extractPoses.sh $VIDEOFILE $OUTFILE $OUTJSON
