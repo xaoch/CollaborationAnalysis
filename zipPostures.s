8dd6cefc2caa3a51fab6cd88636226ddd9fdb2fa
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-04-29 10:25:38 -0400

    Audio Extraction

---
+++ zipPostures.s
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0:30:00
+#SBATCH --mem=2GB
+#SBATCH --job-name=deWarpingVideo
+#SBATCH --mail-type=END
+#SBATCH --mail-user=xavier.ochoa@nyu.edu
+#SBATCH --output=slurm_extractAudio%j.out
+
+DIR=$1
+
+cd $DIR
+tar -zcvf postures.tar.gz postures
+rm -rf postures
