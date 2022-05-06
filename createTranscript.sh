5ca1e6ae06aa955ceb58746bcfaf8d3ba4d18077
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-05-06 18:04:32 -0400

    ASR

--- createTranscript.sh
+++ createTranscript.sh
@@ -8,4 +8,4 @@ source /ext3/miniconda3/bin/activate
 
 conda activate /scratch/xao1/asr/nemo
 
-python /home/xao1/Code/CollaborationAnalysis/createTranscript.py $VIDFILE $DIR $SPEAKERS
+python /home/xao1/Code/CollaborationAnalysis/createTranscript.py -i $VIDFILE -o $DIR -s $SPEAKERS
