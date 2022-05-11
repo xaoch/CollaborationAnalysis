db952ee3ffc6e3d97f91a6206455532bfa07c821
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-05-11 09:39:36 -0400

    FaceDetection

---
+++ faceDetectionMP.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+# For webcam input:
+cap = cv2.VideoCapture("/scratch/xao1/Biochem/Session_1_0930_Sensor_3/ThreeDewarped.mp4")
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH )
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT )
+fps =  cap.get(cv2.CAP_PROP_FPS)
+out = cv2.VideoWriter('out.mp4',cv2.VideoWriter_fourcc('H','2','6','4'), fps, (width,height))
+frame=0
+with mp_face_detection.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5) as face_detection:
+  while cap.isOpened():
+    success, image = cap.read()
+    frame=frame+1
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(image)
+
+    # Draw the face detection annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.detections:
+      for detection in results.detections:
+        mp_drawing.draw_detection(image, detection)
+    # Flip the image horizontally for a selfie-view display.
+    out.write(image)
+    if frame==300:
+      break
+cap.release()
