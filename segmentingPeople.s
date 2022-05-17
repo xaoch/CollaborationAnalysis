b41686fcccde28b5c76d501eb573819e2108fb7e
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-05-17 11:06:34 -0400

    Segmenting People

--- segmentingPeople.s
+++ segmentingPeople.s
@@ -3,12 +3,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:rtx8000:1
-#SBATCH --time=8:00:00
-#SBATCH --mem=4GB
-#SBATCH --job-name=poseExtraction
+#SBATCH --time=2:00:00
+#SBATCH --mem=8GB
+#SBATCH --job-name=segmentingPeople
 #SBATCH --mail-type=END
 #SBATCH --mail-user=xavier.ochoa@nyu.edu
-#SBATCH --output=slurm_poseExtraction%j.out
+#SBATCH --output=slurm_segmentingPeople%j.out
 
 
 VIDEOFILE=$1
@@ -20,5 +20,5 @@ singularity exec --nv --overlay $OVERLAY_FILE $SINGULARITY_IMAGE /bin/bash /home
 
 cd $OUTJSON
 cd ..
-tar -zcvf postures.tar.gz postures
-rm -rf postures
+tar -zcvf labels.tar.gz labels
+rm -rf labels
